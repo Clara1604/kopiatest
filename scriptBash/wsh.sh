18a6c97142ea6a630f4ff2bb04574846b0074433
clara <clara@mini-de-clara.home>
2023-07-06 15:42:15 +0200

    maj wsh.sh

--- scriptBash/wsh.sh
+++ scriptBash/wsh.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-curl -s https://kopia.io/signing-key | sudo gpg --dearmor -o /etc/apt/keyrings/kopia-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kopia-keyring.gpg] http://packages.kopia.io/apt/ stable main" | sudo tee /etc/apt/sources.list.d/kopia.list
-sudo apt update
-sudo apt install kopia
-sudo apt install kopia-ui
+#Variables
+KopiaVersion="0.13.0"
+version="kopia-${KopiaVersion}-linux-x64"
+DownloadURL="https://github.com/kopia/kopia/releases/download/v${KopiaVersion}/${version}.tar.gz"
+# Lien : https://github.com/kopia/kopia/releases/download/v0.13.0/kopia-0.13.0-linux-x64.tar.gz
+
+
+mkdir tmp
+cd tmp/
+mkdir kopia
+cd kopia/
+
+wget $DownloadURL
+tar -xzf ${version}.tar.gz
+
+cd ${version}/
