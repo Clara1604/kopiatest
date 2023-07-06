5907f374689eda3fb50edf146c6ad521d6def4da
clara <clara@mini-de-clara.home>
2023-07-06 15:48:02 +0200

    amélioration  wsh.sh

--- scriptBash/wsh.sh
+++ scriptBash/wsh.sh
@@ -7,12 +7,22 @@ DownloadURL="https://github.com/kopia/kopia/releases/download/v${KopiaVersion}/$
 # Lien : https://github.com/kopia/kopia/releases/download/v0.13.0/kopia-0.13.0-linux-x64.tar.gz
 
 
-mkdir tmp
-cd tmp/
+# Création du répertoire 
+mkdir app
+cd app/
 mkdir kopia
 cd kopia/
 
+# Téléchargement de Kopia
 wget $DownloadURL
+
+# Extraction de l'archive
 tar -xzf ${version}.tar.gz
 
 cd ${version}/
+
+# Affichage du chemin vers l'exécutable Kopia
+echo "Kopia a été installé dans /app/kopia"
+
+# Exemple d'utilisation :
+echo "Pour utiliser Kopia, exécutez /app/kopia/kopia"
