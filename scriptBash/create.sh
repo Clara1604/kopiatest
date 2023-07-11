5019ef86c93ad365b1a765de706788a2f20bc93a
clara <clara@mini-de-clara.home>
2023-07-11 15:38:09 +0200

    ajout create.sh2

--- scriptBash/create.sh
+++ scriptBash/create.sh
@@ -2,4 +2,5 @@
 cd app/
 cd kopia-0.13.0-linux-x64/
 
+
 ./kopia repository create s3 --bucket=clara-main --access-key=Q1L8AYIYDJS75A08EL1T --secret-access-key=QVDquJAVQaFd0B1KYz7xTy9pOlRWPixJxA4Lqg4b --endpoint=cellar-c2.services.clever-cloud.com
