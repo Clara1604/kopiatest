15a4442ffcc5f0c171dd145b98f72cce17c8380f
clara <clara@mini-de-clara.home>
2023-07-12 16:07:20 +0200

    test mdp

--- scriptBash/create.sh
+++ scriptBash/create.sh
@@ -3,6 +3,5 @@ cd app/
 cd kopia-0.13.0-linux-x64/
 
 
-./kopia repository create s3 --bucket=clara-main --access-key=Q1L8AYIYDJS75A08EL1T --secret-access-key=QVDquJAVQaFd0B1KYz7xTy9pOlRWPixJxA4Lqg4b --endpoint=cellar-c2.services.clever-cloud.com
+./kopia repository create s3 --bucket=clara-main --access-key=Q1L8AYIYDJS75A08EL1T --secret-access-key=QVDquJAVQaFd0B1KYz7xTy9pOlRWPixJxA4Lqg4b --endpoint=cellar-c2.services.clever-cloud.com << fic.txt 
 
-{ echo 'toto'; echo 'toto'; } | grub-mkpasswd-pbkdf2
