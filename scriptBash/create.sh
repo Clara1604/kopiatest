0b9ab02b6d4fd5c82fbf62c917220b91ccab789c
clara <clara@mini-de-clara.home>
2023-07-12 16:26:22 +0200

    modif cron.sh

--- scriptBash/create.sh
+++ scriptBash/create.sh
@@ -5,3 +5,4 @@ cd kopia-0.13.0-linux-x64/
 
 ./kopia repository create s3 --bucket=clara-main --access-key=Q1L8AYIYDJS75A08EL1T --secret-access-key=QVDquJAVQaFd0B1KYz7xTy9pOlRWPixJxA4Lqg4b --endpoint=cellar-c2.services.clever-cloud.com << fic.txt 
 
+#https://forum.hardware.fr/hfr/Programmation/Shell-Batch/saisies-clavier-automatiquement-sujet_85529_1.htm
