8917d997f1bbbcf2618543fda9dea0fca3671d67
clara <clara@mini-de-clara.home>
2023-07-13 14:54:41 +0200

    modif create

--- scriptBash/create.sh
+++ scriptBash/create.sh
@@ -2,7 +2,16 @@
 cd app/
 cd kopia-0.13.0-linux-x64/
 
+/usr/bin/expect <<EOD
+spawn ./kopia repository create s3 --bucket=clara-main --access-key=Q1L8AYIYDJS75A08EL1T --secret-access-key=QVDquJAVQaFd0B1KYz7xTy9pOlRWPixJxA4Lqg4b --endpoint=cellar-c2.services.clever-cloud.com
+expect "Enter password to create new repository: "
+send "clara"
+expect "Re-enter password for verification: "
+send "clara"
+expect eof
+EOD
 
-./kopia repository create s3 --bucket=clara-main --access-key=Q1L8AYIYDJS75A08EL1T --secret-access-key=QVDquJAVQaFd0B1KYz7xTy9pOlRWPixJxA4Lqg4b --endpoint=cellar-c2.services.clever-cloud.com << fic.txt 
+./kopia repository create s3 --bucket=clara-main --access-key=Q1L8AYIYDJS75A08EL1T --secret-access-key=QVDquJAVQaFd0B1KYz7xTy9pOlRWPixJxA4Lqg4b --endpoint=cellar-c2.services.clever-cloud.com
+
 
 #https://forum.hardware.fr/hfr/Programmation/Shell-Batch/saisies-clavier-automatiquement-sujet_85529_1.htm
