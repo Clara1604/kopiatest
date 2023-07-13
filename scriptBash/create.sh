6dcdc86541601d164f01d699fca262e3dede2c18
clara <clara@mini-de-clara.home>
2023-07-13 14:58:03 +0200

    modif \n

--- scriptBash/create.sh
+++ scriptBash/create.sh
@@ -5,9 +5,9 @@ cd kopia-0.13.0-linux-x64/
 /usr/bin/expect <<EOD
 spawn ./kopia repository create s3 --bucket=clara-main --access-key=Q1L8AYIYDJS75A08EL1T --secret-access-key=QVDquJAVQaFd0B1KYz7xTy9pOlRWPixJxA4Lqg4b --endpoint=cellar-c2.services.clever-cloud.com
 expect "Enter password to create new repository: "
-send "clara"
+send "clara\n"
 expect "Re-enter password for verification: "
-send "clara"
+send "clara\n"
 expect eof
 EOD
 
